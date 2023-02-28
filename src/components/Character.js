d6d1c8f73bc176172f2d5f4e9a3be2410ede14d6
Devin Bolden <devinbolden6@gmail.com>
2023-02-28 01:24:17 -0600

    added state/effect/axios and some character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,9 @@
 // Write your Character component here
+import React from "react";
+import axios from 'axios';
+
+
+const CharacterData = (props) => {
+    console.log('this is characterData console log', props)
+}
+export default CharacterData;
