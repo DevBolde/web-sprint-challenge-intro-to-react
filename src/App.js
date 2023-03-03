77e84db9371332f897ae57170834c86611f7cbb9
Devin Bolden <devinbolden6@gmail.com>
2023-03-03 12:36:22 -0600

    progress on character component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
 import React, { useState, useEffect } from 'react';
-import CharacterData from './components/Character';
+import Character from './components/Character';
 import axios from 'axios';
+import styled, {keyframes} from 'styled-components'
+
 
 const App = (props) => {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 const [ characters, setCharacters ] = useState('')
+// const [ birth_year, setBirth_Year ] = useState('')
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-useEffect(() => {
-  
-  axios.get('https://swapi.dev/api/people/') 
-  .then(res => {
-    const resMap = res.data.map(element => {
-      // console.log('element console',element)
-      const charBirth = element.birth_year;
-      const charName = element.name;
 
-      setCharacters(charBirth)
-    })
-    // console.log('res console', res.data)
-      // setCharacters()
+  useEffect(() => {
+ axios.get('https://swapi.dev/api/people/')
+.then(res => {
+  const charMap = res.data.map((ele) => {
+    setCharacters(ele)
+    
   })
-  .catch(err => {
-      console.error(err)
-  })
-
-}, [])
-// console.log('this is props console', props)
+  return charMap;
+})
+.catch(err => {
+  console.error(err)
+})
+}, [props.name])
 
   return (
     
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <CharacterData characters={characters}/>
+      <Character characters={characters.name} />
     
     </div>
   );
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,32 @@
 // Write your Character component here
 import React from "react";
+import styled, {keyframes} from 'styled-components'
 
+const kf = keyframes`
+50% {
+    transform: scale(0.8);
+  }
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
 
-const CharacterData = ({ props }) => {
+`
+const StyledCharacter = styled.div`
+width: 60%;
+display: flex;
+justify-content: space-between;
+padding: 8px;
+border-bottom: 2px solid black;
+`
+
+const Character = (props) => {
+    console.log('logging in character', props)
     return (
     <>
-        <h1>characters {props}</h1>
-        <h2></h2>
-
+        <StyledCharacter>
+            <h2>name: {props.name}</h2>
+        </StyledCharacter>
     </>) 
 }
-export default CharacterData;
+export default Character;
